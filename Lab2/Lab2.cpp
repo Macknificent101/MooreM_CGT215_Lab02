2fee0887b244afa0c7ebc1ab4a7514406f7f021b
Macknificent101 <macknificent101@gmail.com>
2023-08-30 11:55:34 -0400

    Made hte programs requested

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -3,12 +3,25 @@
 using namespace std;
 
 int main() {
+
 	float A;
 	float B;
 	float X;
+
+	cout << "Hello, my name is Ian and I'm going to solve the equation:" << endl;
+	cout << "Ax + b = 0" << endl;
+	cout << "For x" << endl << endl;
+	
 	cout << "Please enter a value for A: ";
 	cin >> A;
-	cout << "A=" << A << endl;
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	
+	cout << endl << "Solving " << A << "x+" << B << "=0" << endl;
+
+	cout << endl << "The answer is:" << endl;
+
+	X = (0 - B) / A;
+	cout << "x="<< X << endl;
 }
